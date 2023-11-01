6f1486f411eea27ff2d4532fbeabeaab2f0cf4be
User <User@LAB3-3>
2023-11-01 10:03:20 +0200

    ..

--- CollectionsExe/Program.cs
+++ CollectionsExe/Program.cs
@@ -7,7 +7,7 @@ namespace CollectionsExe
     internal class Program
     {
         #region LIST
-
+       
         static void Main(string[] args)
         {
             #region Define List
@@ -16,14 +16,14 @@ namespace CollectionsExe
 
             //Define 5 rectangles:
             // using empty c'tor:r1-size (2,3)
-            Rectangle r1 = new Rectangle() { Length = 2, Width = 3 };
+            Rectangle r1 = new Rectangle() { Length=2, Width=3};
 
             // other sizes: r2-(2,3), r-3(3,3),r-4(4,3),r-5(5,3)
             Rectangle r2 = new Rectangle(2, 3);
             Rectangle r3 = new Rectangle(3, 3);
-            Rectangle r4 = new Rectangle(5, 3);
-            Rectangle r5 = new Rectangle(5, 3);
-
+            Rectangle r4 = new Rectangle(5,3);
+            Rectangle r5= new Rectangle(5,3);   
+          
             #endregion
 
             #region Add to List
@@ -40,16 +40,16 @@ namespace CollectionsExe
             //loop through the list using count and the [] operator
 
             Console.WriteLine("loop through the list using count and the [] operator");
-            for (int i = 0; i < rectangles.Count; i++)
+           for(int i = 0;i< rectangles.Count; i++)
             {
                 Console.WriteLine(rectangles[i]);
             }
 
             Console.WriteLine("Loop through the list using foreach");
             //Loop through the list using foreach
-            foreach (Rectangle r in rectangles)
+            foreach(Rectangle r in rectangles)
             {
-                Console.WriteLine(r);
+                Console.WriteLine(r);   
             }
 
             #endregion
@@ -59,32 +59,74 @@ namespace CollectionsExe
             //Delete from a list Remove And RemoveAt
             rectangles.Remove(r3);
             rectangles.RemoveAt(3);
-            Console.WriteLine("after delete r5 and index 0");
+                Console.WriteLine("after delete r5 and index 0");
 
             #endregion
 
             #region Update  a list
 
-                //Update an object in a list
-             
+            //Update an object in a list
+            rectangles[0] = r5;
+            rectangles[0].Length = 4;
                 Console.WriteLine("after placing r5 in index 0");
                 
                 #endregion
 
-            #region transforming list to array and to new list
-            //Copy to array ToArray()
+                #region transforming list to array and to new list
+                //Copy to array ToArray()
+               
+                //Copy ToList()
 
-            //Copy ToList()
-
-            Console.WriteLine("Copied list");
-            //todo
+                Console.WriteLine("Copied list");
+              //todo
 
                 Console.WriteLine("Array");
-                //todo
-                #endregion
-
+            //todo
+            #endregion
+            Rectangle[] rectanglesArr= rectangles.ToArray();
+            List<Rectangle> list = rectangles.ToList();
             
             #endregion
+
+            University university = new University();
+            university.Students = new List<Student>();
+            List<Student> students = new List<Student>();
+            Func<List<>, int, int> Returnsum = (students, kita) =>
+            {
+                int counter = 0;
+                foreach (Student student in students)
+                {
+                    if (student.Kita == kita) counter++;
+                }
+                return counter;
+
+            };
+            Func<List<>, string, Student> Returnstu = (students, name) =>
+            {
+
+                foreach (Student s in students)
+                {
+                    if (s.Name == name)
+                        return s;
+                }
+            };
+            Func<List<>,int,List<>> ReturnList=(students, kita,kitalist) =>
+            {
+                List<Student> kita = new List<Student>();
+                foreach(Student stu in students)
+                {
+                    if (stu.Kita == kita)
+                    {
+                        kita.Add(stu);
+                    }
+                }
+                return kita; 
+            }
+
+        /// בדיקת הקוד
+        /// 
+              Console.WriteLine(Returnsum(list, 2)); //return 11
+              Console.WriteLine(Returnstu(list, "Shira"));
+              Console.WriteLine(ReturnList(list, 2)); 
         }
-    }
 }
